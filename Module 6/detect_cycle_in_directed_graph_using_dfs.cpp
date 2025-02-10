9765bc1080462d00af47ad3391ba629487b3cf6e
Mahmud Pias <mahmudpias9@gmail.com>
2025-02-10 21:56:29 +0600

    Cycle Detection Added

--- Module 6/detect_cycle_in_directed_graph_using_dfs.cpp
+++ Module 6/detect_cycle_in_directed_graph_using_dfs.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 bool vis[105];
 vector<int> adj_list[105];
-int parent[105];
+bool pathvis[105];
 bool cycle;
 
-void bfs(int src)
+void dfs(int src)
 {
-    queue<int> q;
-    q.push(src);
     vis[src] = true;
-    while(!q.empty())
+    pathvis[src] = true;
+    for(int child : adj_list[src])
     {
-        int par = q.front();
-        q.pop();
-        for(int child : adj_list[par])
+        if(vis[child] && pathvis[child])
+            cycle = true;
+        if(!vis[child])
         {
-            if(vis[child] && parent[par]!=child)
-                cycle = true;
-            if(!vis[child])
-            {
-                q.push(child);
-                vis[child] = true;
-                parent[child] = par;
-            }
+            dfs(child);
         }
     }
+    // return
+    pathvis[src] = false;
 }
 
 int main()
@@ -37,15 +31,14 @@ int main()
         int a,b;
         cin >> a >> b;
         adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
     }
     memset(vis,false,sizeof(vis));
-    memset(parent,-1,sizeof(parent));
+    memset(pathvis,false,sizeof(pathvis));
     cycle = false;
     for(int i=0;i<n;i++)
     {
         if(!vis[i])
-            bfs(i);
+            dfs(i);
     }
     if(cycle)
         cout << "Cycle Detected\n";
